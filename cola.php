0cccdfc6511f2f4c50b926ee3d75f8829cdd655a
Mariana13097 <marizaco@uninpahu.edu.co>
2025-05-14 07:34:06 -0500

    Add files via upload

---
+++ cola.php
@@ -0,0 +1,16 @@
+<?php
+	$cola = array();
+	array_push($cola,"Mulan");
+	array_push($cola,"La Princesa y el sapo");
+	array_push($cola,"Blancanieves");
+	array_push($cola,"Toy story");
+	array_push($cola,"Aladdin");
+	array_push($cola,"Thinkerbell");
+	array_push($cola,"La dama y el Vagabundo");
+	array_push($cola,"101 dalmatas");
+	array_push($cola,"Big hero 6");
+	 
+	 $elemento = array_shift($cola);
+	 
+	 echo "el primer elemento que llego la cola es: ".$elemento
+?>
